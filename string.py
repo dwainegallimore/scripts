0728184adf46fa0196ada07f0166441bb9af4f44
Nikhil R <rnikhil2751996@gmail.com>
2015-07-06 18:43:49 +0530

    Add strings

---
+++ string.py
@@ -0,0 +1,12 @@
+__author__ = 'Nikhil'
+
+result = str(raw_input("Enter string:"))
+
+total = 0
+for x in result:
+    if x in "AEIOUaeiou":
+        total += 1
+if total>0:
+    print "There are %d vowels in the input" % total
+else:
+    print "there are no vowels"
